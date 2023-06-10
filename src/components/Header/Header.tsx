8b789d67c6b87832f8c31eeb20ed2fa9719b7d8f
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-06-10 17:16:41 +1200

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css'
+import Logo from '../../assets/logo.png'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={Logo} className='logo' />
+      
+    </div>
+  )
+}
+
+export default Header
